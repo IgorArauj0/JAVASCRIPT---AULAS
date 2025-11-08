7cb72e249bc66b94d25ced4029ff61002772e0cb
IgorArauj0 <igoritoaraujo223@gmail.com>
2025-11-07 21:45:43 -0300

    save

---
+++ aula-03/aula03.js
@@ -0,0 +1,6 @@
+let nome = "Igor";
+let idade = 24;
+
+let mensagem = `Olá, meu nome é ${nome} e tenho ${idade} anos de idade`;
+
+console.log(mensagem); //utilizado para fazer a impressão do resultado na tela 
