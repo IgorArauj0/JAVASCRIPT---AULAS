86192abaa0147d7410bd1e2bdeb62781f542edf6
IgorArauj0 <igoritoaraujo223@gmail.com>
2025-11-11 21:44:35 -0300

    save

---
+++ aula-05/aula05.js
@@ -0,0 +1,22 @@
+//console.log('Mensagem de teste');
+
+//Podemos declarar uma variável de Duas maneiras, usando o LET ou CONST
+//Const = Usado para declarar uma variável de valor fixo, ou seja o valor nunca poderá ser alterado
+
+//Let = É usadoo para declarar uma variável de valor NÃO fixo, ou seja, podemos a qualquer momento alterar o valor dela
+
+const mensagemTela = ('Este texto é um exemplo para podermos praticar');
+//console.log(mensagemTela);
+
+//mensagemTela = ('Eu quero alterar meu texto por algum motivo');
+
+let mensagemExemplo = ('Esta mensagem é um teste');
+//console.log(mensagemExemplo);
+
+mensagemExemplo = ('Estou alterando minha frase por motivos de estresse');
+//console.log(mensagemExemplo);
+
+
+const fruta = 'maça';
+
+console.log(fruta);
